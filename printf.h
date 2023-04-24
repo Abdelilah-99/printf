6b67ce1f5b2305944e5e3aa3b0c56abe5f968dc7
mananahamdaoui <manana.hamdaoui@gmail.com>
2023-04-24 14:43:15 -0700

    partner B

---
+++ printf.h
@@ -0,0 +1,26 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * struct fmt - Struct for formatting data.
+ * @sym: *sym: Pointer to a character string.
+ * @fn: func pointer to a function that takes a va_list as an arg
+*/
+ typedef struct fct
+{
+	char *ict;
+	int (*fn)(va_list)
+}fct_t
+
+int_printf(const char *format, ...);
+int (*get_f(const char *format))(va_list);
+int print_char(va_list ls);
+int print_str(va_list ls);
+int print_perc(va_list ls);
+int _putchar(char c);
+
+#endif
